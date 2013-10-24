c24ea6cbc9c9fedcc0ee28696ef4b80dd066571d
tafypz <sebastien.astie@gmail.com>
2013-10-24 17:28:49 -0400

    Added configurable credentials for test case

--- src/test/groovy/com/tikinou/schedulesdirect/ClientTest.groovy
+++ src/test/groovy/com/tikinou/schedulesdirect/ClientTest.groovy
@@ -1,4 +1,22 @@
+/*
+ * Copyright 2013 Tikinou LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.tikinou.schedulesdirect
+
+import groovy.json.JsonSlurper
+import org.junit.Test
 
 /**
  * Created by sebastien on 10/24/13.
@@ -13,8 +31,15 @@ class ClientTest {
 
     }
 
+    @Test
     void testConnect() {
-
+        def slurper = new JsonSlurper()
+        def config = slurper.parseText( ClientTest.class.getResource( '/credentials.json' ).text )
+        Credentials credentials = new Credentials(username:config.username, password:config.password)
+        Client client = new Client(SchedulesDirectApiVersion.VERSION_20130709)
+        assert credentials.randhash == null
+        client.connect(credentials)
+        assert credentials.randhash != null
     }
 
     void testExecute() {
